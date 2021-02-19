af118076d576319e72896e6994b19706e03c3669
Toktobubu <Junusova@users.noreply.github.com>
2021-02-19 10:56:42 +0600

    Update WebMockServerMethods.kt

--- stub/WebMockServerMethods.kt
+++ stub/WebMockServerMethods.kt
@@ -1,7 +1,7 @@
-package my.beeline.hub.stub
 
-import my.beeline.hub.const.MOCKWEBSERVER
-import my.beeline.hub.const.controller.*
+
+import hub.const.MOCKWEBSERVER
+import hub.const.controller.*
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
@@ -13,9 +13,9 @@ object WebMockServerMethods {
             override fun dispatch(request: RecordedRequest): MockResponse {
                 val response = MockResponse()
                 return when (request.path) {
-                    CHECK_SUPPORT_7051289504 -> return response
+                    CHECK_SUPPORT_5555950997 -> return response
                             .setResponseCode(200)
-                    MY_BEELINE_NUMBER_7051289504 -> return response
+                    MY_BEELINE_NUMBER_5559500 -> return response
                             .setResponseCode(200)
                             .setBody("endpoint")
                     else -> MockResponse().setResponseCode(404)
